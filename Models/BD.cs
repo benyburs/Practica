8e6c7b3a3fc9e55bd934d766237a6840b237f0dc
Benicio Bursztyn <benyburs@gmail.com>
2023-09-17 17:13:59 -0300

    primero

---
+++ Models/BD.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using Dapper;
+
+public static class  BD
+{
+    private static string _connectionString = @"Server=localhost\SQLEXPRESS;Database=GestionAlumnos;Integrated Security=True;";
+
+}
